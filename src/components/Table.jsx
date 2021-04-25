96c3c931b7a8ab77e292f88b383583365ce81734
Christopher Whiteman <chwhitem@microsoft.com>
2021-04-24 22:37:08 -0700

    push dummy data

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -29,9 +29,187 @@ export class Table extends React.Component {
             .catch((err) => {
                 console.error(err);
                 this.setState({
-                    tableData: []
+                    tableData: this.createDummyData()
                 });
             });
+    }
+
+    // TEMPORARY
+    createDummyData() {
+        const { tableName } = this.props;
+
+        if (tableName === "Customers") return [
+            {
+                customerId: 1,
+                businessName: "Willy Wonka",
+                firstName: "Willy",
+                lastName: "Wonka",
+                email: "willyWonka@wonka.com",
+                phoneNumber: "342-324-3244",
+                balance: 150.00,
+                billingStreetLineOne: "12222 Street",
+                billingStreetLineTwo: "",
+                billingCity: "Phoenix",
+                billingState: "New York",
+                billingZip: "34244",
+                cardNumber: 1234123412341234,
+                cardExpMonth: 12,
+                cardExpYear: 2020,
+                businessLicenseNumber: 12345
+            },
+            {
+                customerId: 1,
+                businessName: "Willy Wonka",
+                firstName: "Willy",
+                lastName: "Wonka",
+                email: "willyWonka@wonka.com",
+                phoneNumber: "342-324-3244",
+                balance: 150.00,
+                billingStreetLineOne: "12222 Street",
+                billingStreetLineTwo: "",
+                billingCity: "Phoenix",
+                billingState: "New York",
+                billingZip: "34244",
+                cardNumber: 1234123412341234,
+                cardExpMonth: 12,
+                cardExpYear: 2020,
+                businessLicenseNumber: 12345
+            }
+        ];
+        if (tableName === "Employees") return [
+            {
+                employeeID: 1,
+                firstName: "Oompa",
+                lastName: "Loompa",
+                type: "dunno",
+                warehouseID: 2,
+                email: "someEmail@okay.com",
+                phoneNumber: "332-342-5342"
+            },
+            {
+                employeeID: 1,
+                firstName: "Oompa",
+                lastName: "Loompa",
+                type: "dunno",
+                warehouseID: 2,
+                email: "someEmail@okay.com",
+                phoneNumber: "332-342-5342"
+            },
+            {
+                employeeID: 1,
+                firstName: "Oompa",
+                lastName: "Loompa",
+                type: "dunno",
+                warehouseID: 2,
+                email: "someEmail@okay.com",
+                phoneNumber: "332-342-5342"
+            }
+        ];
+        if (tableName === "Orders") return [
+            {
+                orderNumber: 123,
+                orderDate: "2020-12-20",
+                employeeID: 123,
+                customerID: 123,
+                subTotal: 123.00,
+                taxes: 2.00,
+                total: 125.00,
+            },
+            {
+                orderNumber: 123,
+                orderDate: "2020-12-20",
+                employeeID: 123,
+                customerID: 123,
+                subTotal: 123.00,
+                taxes: 2.00,
+                total: 125.00,
+            },
+            {
+                orderNumber: 123,
+                orderDate: "2020-12-20",
+                employeeID: 123,
+                customerID: 123,
+                subTotal: 123.00,
+                taxes: 2.00,
+                total: 125.00,
+            },
+            {
+                orderNumber: 123,
+                orderDate: "2020-12-20",
+                employeeID: 123,
+                customerID: 123,
+                subTotal: 123.00,
+                taxes: 2.00,
+                total: 125.00,
+            },
+            {
+                orderNumber: 123,
+                orderDate: "2020-12-20",
+                employeeID: 123,
+                customerID: 123,
+                subTotal: 123.00,
+                taxes: 2.00,
+                total: 125.00,
+            },
+            {
+                orderNumber: 123,
+                orderDate: "2020-12-20",
+                employeeID: 123,
+                customerID: 123,
+                subTotal: 123.00,
+                taxes: 2.00,
+                total: 125.00,
+            },
+            {
+                orderNumber: 123,
+                orderDate: "2020-12-20",
+                employeeID: 123,
+                customerID: 123,
+                subTotal: 123.00,
+                taxes: 2.00,
+                total: 125.00,
+            },
+        ];
+        if (tableName === "Products") return [
+            {
+                productID: 1,
+                productName: "Chocolate",
+                productDescription: "Choco choco",
+                productDepartment: "Dunno",
+                mdPrice: 125.00,
+                retailerPrice: 150.00,
+                suggestedRetailPrice: 200.00,
+            },
+            {
+                productID: 2,
+                productName: "Chocolate",
+                productDescription: "Choco choco",
+                productDepartment: "Dunno",
+                mdPrice: 125.00,
+                retailerPrice: 150.00,
+                suggestedRetailPrice: 200.00,
+            },
+            {
+                productID: 3,
+                productName: "Chocolate",
+                productDescription: "Choco choco",
+                productDepartment: "Dunno",
+                mdPrice: 125.00,
+                retailerPrice: 150.00,
+                suggestedRetailPrice: 200.00,
+            },
+        ];
+        if (tableName === "Warehouses") return [
+            {
+                warehouseID: 1,
+                warehouseName: "Factory",
+                streetLineOne: "200th street",
+                streetLineTwo: "some place",
+                city: "Nowhere",
+                state: "Dunno",
+                zip: "12434-43",
+            }
+        ];
     }
 
     handleUpdateClick(index) {
